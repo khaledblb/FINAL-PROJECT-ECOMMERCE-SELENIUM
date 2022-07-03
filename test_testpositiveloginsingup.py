844487f6a1609fc5868ee5451b53ed7fe20401ea
khaledblb <khaledblb@gmail.com>
2022-07-03 14:14:33 +0300

    init commit -m "adding comment tests"

--- test_testpositiveloginsingup.py
+++ test_testpositiveloginsingup.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import random
 import string
-'''
+
 def test_testsignup(driver):
     driver.get("https://automationexercise.com/")
 
@@ -63,6 +63,7 @@ def test_testsignup(driver):
     driver.find_element(By.CSS_SELECTOR, ".btn:nth-child(22)").click()
     htmlElement = driver.find_element(By.CSS_SELECTOR,"#form > div > div > div > h2")
     assert htmlElement.text == "ACCOUNT CREATED!"
+
 '''
 def test_testsignin(driver):
     driver.get("https://automationexercise.com/")
@@ -75,6 +76,6 @@ def test_testsignin(driver):
     driver.implicitly_wait(5)
     htmlElement = driver.find_element(By.CSS_SELECTOR, "#header > div > div > div > div.col-sm-8 > div > ul > li:nth-child(10) > a")
     assert htmlElement.text.find("logged in") == -1
-
+'''
 def random_char(y) :
     return ''.join(random.choice(string.ascii_letters) for x in range(y))
